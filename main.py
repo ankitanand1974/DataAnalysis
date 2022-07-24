72a01135759ae9824b25fb11b864b1c30e57f903
Ankit <ankitanand1974@gmail.com>
2022-07-24 15:50:38 +0530

    error fixed

--- main.py
+++ main.py
@@ -8,16 +8,19 @@ print("     |___________________|     ")
 print("                    ------->by Vasu Brother")
 
 dataset = pd.read_csv("vasu.csv")
-print("1 for salary frequency plot")
-print("2 for department frequency plot")
-print("3 to find employees with maximum salary")
-print("5 to exit")
-j = input("Enter your choice: ")
-if (j==1) :
-    dataset['SALARY'].value_counts().plot(kind='bar' , figsize=(12,12))
-elif (j==2) :
-    dataset['DEPARTMENT_10'].value_counts().plot(kind='bar' , figsize=(12,12))
-elif (j==3) :
-    print(dataset['SALARY'].max())
-#elif (j==5) :
+j=0
+while(j==0) :
+    print("1 for salary frequency plot")
+    print("2 for department frequency plot")
+    print("3 to find employees with maximum salary")
+    print("4 to exit")
+    j = int(input("Enter your choice: "))
+    if (j==1) :
+        dataset['SALARY'].value_counts().plot(kind='bar' , figsize=(12,12))
+    elif (j==2) :
+        dataset['DEPARTMENT_10'].value_counts().plot(kind='bar' , figsize=(12,12))
+    elif (j==3) :
+        print(dataset['SALARY'].max())
+    elif (j==4) :
+        break
     
